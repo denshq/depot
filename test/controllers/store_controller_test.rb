ca3a2c21cca45eb928f5f9d42704d601009f7cd3
denshq <denis.sq@yandex.ru>
2018-05-28 17:26:24 +0400

    rework catalog and cart

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,9 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
-    assert_select '.columns .side a', minimum: 4
-    assert_select '.main .entry', 3
-    assert_select '.entry h3', 'Corsair CX750 v2 80+ Bronze 750 Watt'
+    assert_select '.side_nav ul a', minimum: 4
+    assert_select '.catalog li', 3
+    assert_select '.catalog li h3', 'Corsair CX750 v2 80+ Bronze 750 Watt'
   end
 
 end
